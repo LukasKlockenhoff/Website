61c209b0348a2fea358298d19208c9848eebadc1
lukasklockenhoff <lukasklockenhoff@icloud.com>
2023-11-25 13:12:09 +0100

    fix: footer responsiveness

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -25,7 +25,7 @@ export default async function Footer() {
         <div
             className="w-full md:flex-row items-center mx-auto min-h-fit md:justify-between px-24 bg-white dark:bg-background justify-end sm:pb-0">
             <div className={"flex flex-col sm:flex-row-reverse 2xl:w-2/3 xl:w-[80%] lg:w-full mx-auto justify-between pb-10"}>
-                <div className={"flex justify-evenly md:gap-x-20 w-full md:w-1/3"}>
+                <div className={"flex flex-col sm:flex-row items-center justify-evenly md:gap-x-20 w-full md:w-1/3"}>
                     {links.map((link) => (
                         <Button asChild variant={"ghost"} key={link.name} className={"w-1/3 hover:underline hover:underline-offset-4"}>
                             <Link href={link.url} key={link.name} className={TextStyles.BigHint}>
