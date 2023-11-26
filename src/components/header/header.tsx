d40a2a6bf3171ca73f59850912d22898c7dca63d
lukasklockenhoff <lukasklockenhoff@icloud.com>
2023-11-26 17:45:45 +0100

    feat: new nav

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -4,60 +4,61 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { ModeToggle } from "@/components/ui/theme-toggle";
 import MenuIcon from "@mui/icons-material/Menu";
+import { MobileHeader } from "@/components/header/mobile-header";
 
 import { Menu, Transition } from "@headlessui/react";
 import { Fragment } from "react";
 import Icon from "@/components/ui/icon";
 import { twMerge } from "tailwind-merge";
 import textstyles from "@/utils/textstyles";
-
-function MobileHeader() {
-  return (
-    <div className="flex top-16 text-right border-1 border-border">
-      <Menu as="div" className="relative inline-block text-left">
-        <div>
-          <Menu.Button className="inline-flex w-full justify-center rounded-md px-4 py-2 text-sm font-medium text-white border-border focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75">
-            <MenuIcon className={"dark:text-white text-black"} />
-          </Menu.Button>
-        </div>
-        <Transition
-          as={Fragment}
-          enter="transition ease-out duration-100"
-          enterFrom="transform opacity-0 scale-95"
-          enterTo="transform opacity-100 scale-100"
-          leave="transition ease-in duration-75"
-          leaveFrom="transform opacity-100 scale-100"
-          leaveTo="transform opacity-0 scale-95"
-        >
-          <Menu.Items className="absolute right-0 mt-2 w-44 origin-top-right divide-y divide-gray-100 rounded-md bg-white dark:bg-background shadow-lg ring-1 ring-black/5 focus:outline-none border-1 border-border">
-            <div className="px-1 py-1 flex flex-col items-start w-full rounded-lg">
-              <Menu.Item>
-                <Button asChild variant={"link"}>
-                  <Link
-                    href={"/about"}
-                    className={"text-black dark:text-white"}
-                  >
-                    About
-                  </Link>
-                </Button>
-              </Menu.Item>
-              <Menu.Item>
-                <Button asChild variant={"link"}>
-                  <Link href={"/projects"}>Projects</Link>
-                </Button>
-              </Menu.Item>
-              <Menu.Item>
-                <Button asChild variant={"link"}>
-                  <Link href={"/"}>Home</Link>
-                </Button>
-              </Menu.Item>
-            </div>
-          </Menu.Items>
-        </Transition>
-      </Menu>
-    </div>
-  );
-}
+//
+// function MobileHeader() {
+//   return (
+//     <div className="flex top-16 text-right border-1 border-border">
+//       <Menu as="div" className="relative inline-block text-left">
+//         <div>
+//           <Menu.Button className="inline-flex w-full justify-center rounded-md px-4 py-2 text-sm font-medium text-white border-border focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75">
+//             <MenuIcon className={"dark:text-white text-black"} />
+//           </Menu.Button>
+//         </div>
+//         <Transition
+//           as={Fragment}
+//           enter="transition ease-out duration-100"
+//           enterFrom="transform opacity-0 scale-95"
+//           enterTo="transform opacity-100 scale-100"
+//           leave="transition ease-in duration-75"
+//           leaveFrom="transform opacity-100 scale-100"
+//           leaveTo="transform opacity-0 scale-95"
+//         >
+//           <Menu.Items className="absolute right-0 mt-2 w-44 origin-top-right divide-y divide-gray-100 rounded-md bg-white dark:bg-background shadow-lg ring-1 ring-black/5 focus:outline-none border-1 border-border">
+//             <div className="px-1 py-1 flex flex-col items-start w-full rounded-lg">
+//               <Menu.Item>
+//                 <Button asChild variant={"link"}>
+//                   <Link
+//                     href={"/about"}
+//                     className={"text-black dark:text-white"}
+//                   >
+//                     About
+//                   </Link>
+//                 </Button>
+//               </Menu.Item>
+//               <Menu.Item>
+//                 <Button asChild variant={"link"}>
+//                   <Link href={"/projects"}>Projects</Link>
+//                 </Button>
+//               </Menu.Item>
+//               <Menu.Item>
+//                 <Button asChild variant={"link"}>
+//                   <Link href={"/"}>Home</Link>
+//                 </Button>
+//               </Menu.Item>
+//             </div>
+//           </Menu.Items>
+//         </Transition>
+//       </Menu>
+//     </div>
+//   );
+// }
 
 export default function Header() {
   return (
---
+++ src/components/header/mobile-header.tsx
@@ -0,0 +1,101 @@
+import { Dialog, Transition } from "@headlessui/react";
+import { Fragment, useState } from "react";
+import { Button } from "@/components/ui/button";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
+import textstyles from "@/utils/textstyles";
+import { twMerge } from "tailwind-merge";
+import Link from "next/link";
+import Divider from "@mui/material/Divider";
+
+export function MobileHeader() {
+  let [isOpen, setIsOpen] = useState(true);
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  return (
+    <>
+      <div className="inset-0 flex items-center justify-center">
+        <Button
+          type="button"
+          variant={"ghost"}
+          size={"icon"}
+          onClick={openModal}
+        >
+          <MenuIcon className={"dark:text-white text-black"} />
+        </Button>
+      </div>
+
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={closeModal}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black/25" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex h-1/3 items-center mt-6 p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white dark:bg-background p-6 text-left align-middle shadow-xl transition-all">
+                  <div className="flex justify-between items-center">
+                    <Dialog.Title as="h3" className={twMerge(textstyles.Text)}>
+                      Navigation
+                    </Dialog.Title>
+                    <Button
+                      size={"icon"}
+                      onClick={closeModal}
+                      variant={"ghost"}
+                    >
+                      <CloseIcon className={twMerge(textstyles.Text)} />
+                    </Button>
+                  </div>
+                  <div className="items-start flex flex-col">
+                    <Divider variant={"fullWidth"} />
+                    <Button asChild variant={"ghost"} className={""}>
+                      <Link href={"/about"} className={textstyles.Text}>
+                        About
+                      </Link>
+                    </Button>
+                    <Divider />
+                    <Button asChild variant={"ghost"} className={""}>
+                      <Link href={"/projects"} className={textstyles.Text}>
+                        Projects
+                      </Link>
+                    </Button>
+                    <Divider />
+                    <Button asChild variant={"ghost"} className={""}>
+                      <Link href={"/"} className={textstyles.Text}>
+                        Home
+                      </Link>
+                    </Button>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
+    </>
+  );
+}
