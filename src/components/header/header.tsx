462d4d732821b01cd136331b6297447bda13db75
lukasklockenhoff <lukasklockenhoff@icloud.com>
2024-09-19 12:35:34 +0200

    simplify header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -17,7 +17,7 @@ export default function Header() {
           "flex py-10 w-[90%] sm:w-2/3 2xl:w-2/3 xl:w-[80%] items-center justify-between mx-auto"
         }
       >
-        <div className={"w-1/3 hidden md:flex"}>
+        <div className={"w-1/3 flex"}>
           <div className={"flex w-2/3 gap-x-4 lg:gap-x-0"}>
             <Button asChild variant={"ghost"} className={"w-1/2"}>
               <Link href={"/"}>
@@ -77,71 +77,7 @@ export default function Header() {
             </Button>
           </div>
         </div>
-        <div className={"w-full justify-start md:hidden flex"}>
-          <div className={"w-1/3 flex md:hidden"}>
-            <div className={"flex w-2/3 gap-x-4 lg:gap-x-0"}>
-              <Button asChild variant={"ghost"} className={"w-1/2"}>
-                <Link href={"/"}>
-                  <p
-                    className={twMerge(
-                      Textstyles.LinkText,
-                      currentPath === "/"
-                        ? "!underline decoration-secondary decoration-1"
-                        : "",
-                    )}
-                  >
-                    Home
-                  </p>
-                </Link>
-              </Button>
-              <Button asChild variant={"ghost"} className={"w-1/2"}>
-                <Link href={"/about"}>
-                  <p
-                    className={twMerge(
-                      Textstyles.LinkText,
-                      currentPath === "/about"
-                        ? "!underline decoration-secondary decoration-1"
-                        : "",
-                    )}
-                  >
-                    About
-                  </p>
-                </Link>
-              </Button>
-              <Button asChild variant={"ghost"} className={"w-1/2"}>
-                <Link href={"/projects"}>
-                  <p
-                    className={twMerge(
-                      Textstyles.LinkText,
-                      currentPath === "/projects"
-                        ? "!underline decoration-secondary decoration-1"
-                        : "",
-                    )}
-                  >
-                    Projects
-                  </p>
-                </Link>
-              </Button>
-              <Button asChild variant={"ghost"} className={"w-1/2"}>
-                <Link href={"/photos"}>
-                  <p
-                    className={twMerge(
-                      Textstyles.LinkText,
-                      currentPath === "/photos"
-                        ? "!underline decoration-secondary decoration-1"
-                        : "",
-                    )}
-                  >
-                    Photos
-                  </p>
-                </Link>
-              </Button>
-            </div>
-          </div>
-        </div>
-        <div className={"flex w-1/4 md:w-1/3 justify-end"}>
-          <ModeToggle />
-        </div>
+        <ModeToggle />
       </div>
     </div>
   );
