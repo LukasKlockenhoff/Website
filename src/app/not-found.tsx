7d8fc089ba1ad739754b22f9168b979ff60a7f39
lukasklockenhoff <lukasklockenhoff@icloud.com>
2024-10-03 20:57:52 +0200

    trying it back in not-found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,58 @@
-import { redirect } from "next/navigation";
+import { Suspense } from "react";
+import { twMerge } from "tailwind-merge";
+import Textstyles from "@/utils/textstyles";
+import dynamic from "next/dynamic";
+import SpotlightsBackground from "@/components/not-found/spotlights-background";
 
-// fixes prerender Error
-export const dynamic = "force-dynamic";
+const DynamicSpotlightCircle = dynamic(
+  () => import("@/components/not-found/spotlights-cirlce"),
+  {
+    ssr: false,
+  },
+);
 
-export default function NotFound() {
-  return <div>Not Found</div>;
+const text =
+  "Enim occaecat pariatur officia ad duis mollit pariatur magna esse culpa tempor proident. Sunt aliqua dolore ex eiusmod do id aliqua laboris exercitation reprehenderit ea cupidatat non. Ea labore ea ad cillum velit duis cillum. Aute enim cupidatat laborum veniam tempor occaecat exercitation in pariatur commodo cupidatat. \
+Dolor reprehenderit qui in enim cupidatat. Ad magna tempor dolore enim duis ut id voluptate quis dolor. Deserunt ullamco ut eiusmod eu aute non commodo sunt. Laborum magna adipisicing labore adipisicing. Fugiat ea fugiat eu sit ipsum velit quis culpa occaecat nostrud consequat. Pariatur nisi anim amet laboris minim ad in. \
+Proident fugiat exercitation sit exercitation sunt labore occaecat laborum commodo aute commodo anim. Fugiat laborum aute adipisicing fugiat reprehenderit id id tempor proident. Id exercitation mollit veniam occaecat culpa laborum ipsum nostrud culpa cupidatat veniam et. Elit qui nisi velit officia ex consequat nisi commodo nulla. Nisi officia laboris consequat eiusmod duis voluptate aliqua eu do culpa amet non mollit non. Id anim qui irure dolore anim sit dolor labore officia. \
+Consequat sit pariatur adipisicing nostrud magna sunt amet proident consequat. Commodo adipisicing nulla laboris sunt laboris enim quis proident Lorem aliquip reprehenderit esse do. Ipsum cillum laborum anim amet do commodo labore cillum deserunt. Dolore cupidatat exercitation ea sit velit anim in nostrud magna fugiat officia minim consequat. Sint dolor culpa non sit enim nostrud ut irure minim ullamco aliqua esse. Enim sunt deserunt tempor ex officia voluptate nulla est consectetur velit irure velit amet esse. Reprehenderit laborum culpa excepteur amet ut culpa. \
+Sunt esse anim ullamco adipisicing aute reprehenderit fugiat deserunt sit. Ullamco ipsum commodo exercitation sit proident mollit labore. Dolore do nostrud amet cillum consectetur esse aliqua qui dolor exercitation exercitation velit enim. Dolor enim in pariatur amet aliqua aliquip tempor ullamco aliquip eiusmod mollit et occaecat aliquip. Adipisicing quis in enim consectetur ea fugiat cillum consectetur reprehenderit. Ullamco eu irure sit culpa. Id ad qui laboris magna esse cupidatat. Aliquip officia laboris reprehenderit. \
+Incididunt esse labore laboris irure occaecat elit fugiat. Excepteur excepteur anim sint. Velit qui deserunt nulla est id est veniam aliquip cupidatat velit aliquip aute aliqua incididunt incididunt. Id ipsum ea et. \
+Duis aute pariatur laborum reprehenderit veniam ea ut quis nostrud consequat magna aliqua nisi. Culpa aliquip dolore est elit id ullamco minim. Qui sint nulla voluptate minim sint sunt ipsum. Incididunt ea sint dolor occaecat reprehenderit occaecat cupidatat consectetur amet nulla esse sit.";
+
+export default function Page() {
+  return (
+    <div className="w-full h-full justify-center dark:bg-background bg-white overflow-x-clip">
+      <div className="w-[90%] h-full sm:w-2/3 2xl:w-2/3 xl:w-[80%] mx-auto flex flex-col justify-center my-3 relative">
+        <Suspense fallback={<div>Loading...</div>}>
+          <SpotlightsBackground />
+        </Suspense>
+        <div
+          className={twMerge(
+            "max-w-[600px] justify-center mx-auto w-full select-none z-10",
+          )}
+        >
+          <div
+            className={twMerge(
+              Textstyles.SerifHeader,
+              "!text-white dark:!text-background",
+            )}
+          >
+            This page does not exist (yet)
+          </div>
+          <div
+            className={twMerge(
+              Textstyles.BigHint,
+              "!text-white dark:!text-background",
+            )}
+          >
+            {text}
+          </div>
+        </div>
+      </div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <DynamicSpotlightCircle position="absolute" />
+      </Suspense>
+    </div>
+  );
 }
